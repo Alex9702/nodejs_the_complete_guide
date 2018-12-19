98ccafa908d231bba8ab7357b8bb644956c2ce69
alex9702 <alexsandromoreir@gmail.com>
2018-12-19 16:08:38 -0300

    primeiro commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes.routes);
-app.use('/admin', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next)=>{
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
