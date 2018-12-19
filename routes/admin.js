d1ae7381328c9119e68b72b358d23ed4aff70aba
alex9702 <alexsandromoreir@gmail.com>
2018-12-19 15:58:44 -0300

    primeiro commit

---
+++ routes/admin.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/current_path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res)=>{
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product');
+});
+
+router.get('/add-product', (req, res)=>{
+    products.push({title: req.body.title});
+    res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
